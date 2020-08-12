f68c3f4a5cd2a1c3d23c342bf13b0ee9ab49aca2
Tushar Amrit <48964465+tushar-amrit-6@users.noreply.github.com>
2020-08-12 15:00:15 +0530

    Add files via upload
    initial commit

---
+++ fibonacci.c
@@ -0,0 +1,27 @@
+#include <pthread.h>
+#include <stdio.h>
+int value = 0;
+void *runner(void *param);
+int main(int argc, char * argv[])
+{
+int pid;
+pthread_t tid;
+pthread_attr_t attr;
+  pid = fork();
+  
+  if (pid == 0) {
+     pthread_attr_init(&attr);
+     pthread_create(&tid,&attr,runner,NULL);
+     pthread_join(tid,NULL);
+     printf(“CHILD: value = %d”, value); 
+  }
+  else if (pid > 0) { 
+     wait(NULL);
+     printf(“PARENT: value = %d”, value); 
+  }
+}
+void *runner(void *param) {
+  value = 5;
+  pthread_exit(0);
+}
+
