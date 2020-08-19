e86f445f056251232188f8bbfac417169ed91f3b
Tushar Amrit <48964465+tushar-amrit-6@users.noreply.github.com>
2020-08-19 15:47:58 +0530

    Add files via upload

---
+++ binaryTree.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+    pid_t child_pid;
+    child_pid=fork();
+    if(child_pid==0){
+        child_pid=fork();
+        if(child_pid==0){
+            child_pid=fork();
+        }
+    }
+    else{
+        child_pid= 0;
+        child_pid=fork();
+        if(child_pid==0){
+            child_pid=fork();
+            if(child_pid>0){
+                fork();
+            }
+        }
+    }
