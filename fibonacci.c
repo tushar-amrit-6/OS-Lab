53e3615c87534158118fee1fc427c78d899f14be
Tushar Amrit <48964465+tushar-amrit-6@users.noreply.github.com>
2020-08-19 15:47:22 +0530

    Update fibonacci.c

--- fibonacci.c
+++ fibonacci.c
@@ -1,27 +1,31 @@
-#include <pthread.h>
-#include <stdio.h>
-int value = 0;
-void *runner(void *param);
-int main(int argc, char * argv[])
+#include<iostream>
+#include<pthread.h>
+void* fibonacci(void*);
+using namespace std;
+int n;
+void* fibonnacci(void* arg)
 {
-int pid;
-pthread_t tid;
-pthread_attr_t attr;
-  pid = fork();
-  
-  if (pid == 0) {
-     pthread_attr_init(&attr);
-     pthread_create(&tid,&attr,runner,NULL);
-     pthread_join(tid,NULL);
-     printf(“CHILD: value = %d”, value); 
-  }
-  else if (pid > 0) { 
-     wait(NULL);
-     printf(“PARENT: value = %d”, value); 
-  }
+int c, first = 0, second = 1, next;
+for ( c = 0 ; c <n; c++ )
+{
+if ( c <= 1 )
+next = c;
+else
+{
+next = first + second;
+first = second;
+second = next;
 }
-void *runner(void *param) {
-  value = 5;
-  pthread_exit(0);
+cout << next << endl;
+}
 }
 
+int main()
+{
+pthread_t t;
+cout << "Enter the number of terms of Fibonacci series you want" << endl;
+cin >> n;
+cout << "First " << n << " terms of Fibonacci series are :- " << endl;
+pthread_create (&t , NULL , fibonacci,(void*)&n);
+return 0;
+}
